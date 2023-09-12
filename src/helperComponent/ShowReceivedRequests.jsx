b2e34455805ee587710667e462681d4d961501cd
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-09-12 19:51:32 +0300

    Update ShowReceivedRequests.jsx

--- src/helperComponent/ShowReceivedRequests.jsx
+++ src/helperComponent/ShowReceivedRequests.jsx
@@ -5,7 +5,7 @@ import OtherUserProfile from "../pages/OtherUserProfile";
 import { useReceivedRequestsContext } from "../context/ReceivedRequestsContext";
 import Loader from "../helperComponent/Loader";
 
-import "../CSS/ShowReceivedRequests.css";
+import "../CSS/showReceivedRequests.css";
 
 const ShowReceivedRequests = () => {
   const { isLoading, error, acceptRequest, declineRequest } = useRequest();
