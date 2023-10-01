c7cae45e2226e628b141e81207f12e2dffb16fa3
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-10-01 18:15:44 +0300

    securing the password that benig in the login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,44 +2,45 @@ import { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
 import { FaGoogle } from "react-icons/fa";
 import { useForgotPasswordContext } from "../context/ForgotPasswordContext";
-
-
+import bcrypt from 'bcryptjs'; // Import bcrypt library for password hashing
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { setShowForgotPassword } = useForgotPasswordContext();
 
-  const {login, googleLogin, error, isLoading} = useLogin();
+  const { login, googleLogin, error, isLoading } = useLogin();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    await login(email, password);
-    
+    // Hash the password before sending it to the server
+    const hashedPassword = bcrypt.hashSync(password, 10); // You can adjust the salt rounds as needed
+
+    await login(email, hashedPassword); // Send the hashed password to the login function
   }
 
   return (
     <div className="login">
       <div>
         <div className="group">
-        <label>Email</label>
-        <input 
+          <label>Email</label>
+          <input 
             type="text" 
             name="Email"
             value={email}
             placeholder="Enter your Email"
             onChange={(event) => {setEmail(event.target.value)}}
-        />
+          />
         </div>
         <div className="group">
           <label>Password</label>
           <input 
-              type="password"
-              name="password"
-              value={password}
-              placeholder="Enter your password"
-              onChange={(event) => {setPassword(event.target.value)}}
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={(event) => {setPassword(event.target.value)}}
           />
         </div>
 
@@ -55,4 +56,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
